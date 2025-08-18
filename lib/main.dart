abfa267817c188e95a4ba9f1c2600061995ba7f2
Romyadhy <romyadhy@student.undiksha.ac.id>
2025-08-18 11:30:40 +0800

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'dart:math' as math;
+// import 'dart:math' as math;
 import 'firebase_options.dart';
 
 Future<void> main() async {
